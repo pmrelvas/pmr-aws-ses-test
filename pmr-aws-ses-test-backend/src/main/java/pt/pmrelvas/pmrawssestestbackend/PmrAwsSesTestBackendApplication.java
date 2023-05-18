ac7ed950024daccb43f6f4b1634f5d545c37fb8f
Pedro Relvas <pedromiguelrelvas@gmail.com>
2023-05-18 20:11:27 +0100

    initial commit

---
+++ pmr-aws-ses-test-backend/src/main/java/pt/pmrelvas/pmrawssestestbackend/PmrAwsSesTestBackendApplication.java
@@ -0,0 +1,13 @@
+package pt.pmrelvas.pmrawssestestbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmrAwsSesTestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmrAwsSesTestBackendApplication.class, args);
+	}
+
+}
---
+++ pmr-aws-ses-test-backend/src/test/java/pt/pmrelvas/pmrawssestestbackend/PmrAwsSesTestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.pmrelvas.pmrawssestestbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmrAwsSesTestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
